862b0eb1a4d82e58c9bae58403694b1d33cc4c11
John <fang97j@gmail.com>
2020-04-25 19:09:28 -0400

    Discriminator attempt (also untested)

--- models.py
+++ models.py
@@ -110,14 +110,18 @@ class Model():
         mean_loss = np.mean(losses)
         return mean_loss
 
-def res_block():
-    return nn.Sequential(
-        nn.Conv2D(64, 64, 3, padding=1),
-        nn.BatchNorm2D(64),
-        nn.PReLU(),
-        nn.Conv2D(64, 64, 3, padding=1),
-        nn.BatchNorm2D(64),
-    )
+class ResidualBlock(nn.Module):
+    def __init__(self):
+        res_block = nn.Sequential(
+            nn.Conv2D(64, 64, 3, padding=1),
+            nn.BatchNorm2D(64),
+            nn.PReLU(),
+            nn.Conv2D(64, 64, 3, padding=1),
+            nn.BatchNorm2D(64),
+        )
+
+    def forward(self, x):
+        return x + res_block(x)
 
 def shuffle_block():
     return nn.Sequential(
@@ -127,16 +131,15 @@ def shuffle_block():
     )
 
 class Generator(nn.Module):
-
     def __init__(self):
         super(Generator, self).__init__()
-        self.B = #TODO
-        self.U = 2
+        self.B = 16
+        self.S = 2
 
         self.conv1      = nn.Sequential(nn.Conv2D(3, 64, 9, padding=4), nn.PReLU())
-        self.res_blocks = [res_block() for _ in range(self.B)]
+        self.res_blocks = nn.Sequential(*[ResidualBlock() for _ in range(self.B)])
         self.conv2      = nn.Sequential(nn.Conv2D(64, 64, 3, padding=1), nn.BatchNorm2D(64))
-        self.shuffle    = nn.Sequential(*[shuffle_block() for _ in range(U)])
+        self.shuffle    = nn.Sequential(*[shuffle_block() for _ in range(self.S)])
         self.conv3      = nn.Conv2D(64, 3, 9, padding=4)
 
     def forward(self, x):
@@ -145,18 +148,58 @@ class Generator(nn.Module):
         - x ([batch_size, 3, H, W] Tensor): batch of images to super-resolve
 
         Returns:
-        - [batch_size, 3, 4*H, 4*W] Tensor
+        - [batch_size, 3, 4H, 4W] Tensor
         """
         x = image.float().to(device)
 
         x = self.conv1(x)
         saved_x = x
-
-        for r_b in self.res_blocks:
-            x = r_b(x) + x
-
+        x = self.res_blocks(x)
         x = self.conv2(x) + saved_x
         x = self.shuffle(x)
         x = self.conv3(x)
+        return x
 
+def conv_block(in_channels, out_channels, kernel_size, stride, padding):
+    return nn.Sequential(
+        nn.Conv2D(in_channels, out_channels, kernel_size, stride=stride, padding=padding),
+        nn.BatchNorm2D(out_channels),
+        nn.LeakyReLU(0.2)
+    )
+
+def conv_size(x_in, kernel_size, stride, padding):
+    return floor((x_in + 2 * padding - kernel_size) / stride + 1)
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        in_channels = 3
+        im_width = 96
+
+        layers = []
+        for idx, out_channels in enumerate([64, 128, 256, 512]):
+            if idx == 0:
+                layers.extend([nn.Conv2D(in_channels, out_channels, 3, 1, 1), nn.LeakyReLU(0.2)])
+            else:
+                layers.append(conv_block(in_channels, out_channels, 3, 1, 1))
+
+            layers.append(conv_block(out_channels, out_channels, 3, 2, 1))
+
+            im_width = conv_size(im_width, 3, 1, 1)
+            im_width = conv_size(im_width, 3, 2, 1)
+            in_channels = out_channels
+
+        self.convs = nn.Sequential(*layers)
+
+        self.fc = nn.Sequential(
+            nn.Linear(im_width * im_width * 512, 1024, 1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2D(1024, 1, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = self.fc(x)
         return x
