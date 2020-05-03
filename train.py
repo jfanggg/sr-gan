c4007b6f82c6292a1cff2fa890fc6984678f3101
John <fang97j@gmail.com>
2020-05-03 19:05:49 -0400

    Minor code cleanup

--- train.py
+++ train.py
@@ -10,42 +10,13 @@ from torch.utils import data
 
 def parse_args():
     parser = argparse.ArgumentParser(description='main.py')
-    parser.add_argument(
-        '--model',
-        type=str,
-        default=None,
-        help='Name of file to load model from')
-    parser.add_argument(
-        '--eval_epochs',
-        type=int,
-        default=5,
-        help='How often (in epochs) to evaluate model')
-    parser.add_argument(
-        '--save_epochs',
-        type=int,
-        default=5,
-        help='How often (in epochs) to save model')
-    parser.add_argument(
-        '--data_dir',
-        type=str,
-        default='data',
-        help='Name of directory to get image data')
-    parser.add_argument(
-        '--save_dir',
-        type=str,
-        default='saved_models',
-        help='Name of directory to save models')
-    parser.add_argument(
-        '--seed',
-        type=int,
-        default=0,
-        help='RNG seed')
-    parser.add_argument(
-        '--epochs',
-        type=int,
-        default=100,
-        help='Number of epochs to train for')
-
+    parser.add_argument('--model', type=str, default=None, help='Name of file to load model from')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train for')
+    parser.add_argument('--eval_epochs', type=int, default=5, help='How often (in epochs) to evaluate model')
+    parser.add_argument('--save_epochs', type=int, default=5, help='How often (in epochs) to save model')
+    parser.add_argument('--data_dir', type=str, default='data', help='Name of directory to get image data')
+    parser.add_argument('--save_dir', type=str, default='saved_models', help='Name of directory to save models')
+    parser.add_argument('--seed', type=int, default=0, help='RNG seed')
     args = parser.parse_args()
     return args
 
@@ -57,12 +28,10 @@ def main():
     random.seed(args.seed)
     np.random.seed(args.seed)
 
-    datasets = {
-        key: ImageDataset(os.path.join(args.data_dir, key)) for key in ['train', 'val' , 'test']
-    }
-    dataloaders = {
-        key: data.DataLoader(ds, batch_size=4, shuffle=True, num_workers=4) for key, ds in datasets.items()
-    }
+    datasets, dataloaders = {}, {}
+    for key in ['train', 'val' , 'test']:
+        datasets[key] = ImageDataset(os.path.join(args.data_dir, key))
+        dataloaders[key] = data.DataLoader(datasets[key], batch_size=4, shuffle=True, num_workers=4)
 
     model = Model(args)
     model.train(dataloaders)
